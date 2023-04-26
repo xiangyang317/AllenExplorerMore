55c72d50a9671e9e2cd8c7589d1f011dd478a4f2
xiangyang317 <xiangyang317@gmail.com>
2023-04-27 00:30:56 +0800

    AllenExplorerMore

---
+++ GitHub.bat
@@ -0,0 +1,7 @@
+"C:\Program Files\Git\bin\git.exe" lfs track .
+"C:\Program Files\Git\bin\git.exe" add .gitattributes
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "AllenExplorerMore"
+"C:\Program Files\Git\bin\git.exe" branch -M main
+"C:\Program Files\Git\bin\git.exe" config --global --unset http.proxy
+"C:\Program Files\Git\bin\git.exe" push -u origin main
